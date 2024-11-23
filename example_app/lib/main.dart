07dde67a4f41c09985334cbae18b42e47ec32a16
Majid <mhadaily@gmail.com>
2024-11-23 15:19:03 +0100

    conditional import

--- example_app/lib/main.dart
+++ example_app/lib/main.dart
@@ -4,7 +4,6 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:app/firebase_options.dart';
@@ -15,9 +14,6 @@ import 'package:structure/structure.dart'
     if (kIsWeb) 'package:awesome/awesome.dart';
 
 Future<void> main() async {
-  if (kIsWeb) {
-    debugPrint(Awesome().dogBark());
-  }
   debugPrint(Structure().toString());
   runZonedGuarded(() async {
     WidgetsFlutterBinding.ensureInitialized();
